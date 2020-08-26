902805ecaa675d3baf7a536f7d1a9e296673654b
patorma <pocontreras@ing.ucsc.cl>
2020-08-26 01:45:08 -0400

    se empezo a configurar el proyecto de recetas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //funcion que toma como parametro la vista a mostrar
     return view('welcome');
 });
+
+Route::get('/recetas', 'RecetaCopntroller');
+ //funcion que toma como parametro la vista a mostrar
+    //return view('nosotros');
